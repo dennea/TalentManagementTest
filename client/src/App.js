0a5a14af0403051f8bfb412498bb8106da3e6090
Dennea Maccallum <denneamaccallum@Denneas-MacBook-Air.local>
2022-09-11 11:58:58 -0600

    Added Routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@
 import React from 'react';
 import ButtonAppBar from "./components/AppBar.js";
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from './components/Register.js';
+import Login from './components/Login.js';
+import Landing from './components/LandingPage.js';
 
 // https://mui.com/material-ui/react-dialog/ <- look into dialogs 
 
 function App() {
   return (
-    <ButtonAppBar />
+    <Router>
+      <ButtonAppBar />
+      <Routes>
+        <Route path="/register" element = {<Register />} />
+        <Route path="/login" element = {<Login />} />
+        <Route path="/" element = {<Landing />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,29 +1,45 @@
 import * as React from 'react';
-import { AppBar, Box,Toolbar, Typography, Button, IconButton} from '@material-ui/core';
+import { AppBar, CssBaseline, Box,Toolbar, Typography, Button, IconButton} from '@material-ui/core';
 import MenuIcon from "@material-ui/icons/Menu";
+import { useNavigate } from "react-router-dom";
 
 export default function ButtonAppBar() {
+  let navigate = useNavigate(); 
+  const handleRegisterClick = () => {
+    navigate('/register', {replace: true});
+  };
+  const handleLoginClick = () => {
+    navigate('/login', {replace: true});
+  };
+  const handleHomeClick = () => {
+    navigate('/', {replace: true});
+  };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static"  color='default'>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Talent Management
-          </Typography>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Sign Up</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <div>
+      <div>
+       <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static"  color='default'>
+          <CssBaseline />
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Talent Management
+              </Typography>
+              <Button color="inherit" onClick={handleHomeClick}>Home</Button>
+              <Button color="inherit" onClick={handleLoginClick}>Login</Button>
+              <Button color="inherit" onClick={handleRegisterClick}>Sign Up</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </div>
+    </div>
   );
 }
